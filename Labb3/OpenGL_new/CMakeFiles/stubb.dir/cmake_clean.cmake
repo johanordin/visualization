e3017f27d20316c9fca1e0550c3dbca3d3ea818c
johno855 <johno855@student.liu.se>
2014-11-28 23:08:43 +0100

    started out on labb3
    Signed-off-by: johno855 <johno855@student.liu.se>

---
+++ Labb3/OpenGL_new/CMakeFiles/stubb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stubb.dir/stubb.cpp.o"
+  "stubb.pdb"
+  "stubb"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stubb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ Labb3/textureExample/CMakeFiles/textureExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/textureExample.dir/main.cpp.o"
+  "textureExample.pdb"
+  "textureExample"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/textureExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
