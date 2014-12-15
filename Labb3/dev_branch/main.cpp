a0f0fb860d65852017dcd668ea897130335c6001
johno855 <johno855@student.liu.se>
2014-12-15 16:23:41 +0100

    main filen uppdaterad med senaste versionen --> transformation och rotation fungerar --> maste minska storleken

--- Labb3/dev_branch/main.cpp
+++ Labb3/dev_branch/main.cpp
@@ -6,7 +6,7 @@
 sgct::Engine * gEngine;
 
 //-----------------------
-// function declarations 
+// function declarations
 //-----------------------
 void myInitOGLFun();
 void myPreSyncFun();
@@ -21,7 +21,7 @@ void drawWireCube(float size);
 
 
 //-----------------------
-// variable declarations 
+// variable declarations
 //-----------------------
 sgct_utils::SGCTSphere * mySphere = NULL;
 sgct_utils::SGCTSphere * mySphere1 = NULL;
@@ -41,10 +41,6 @@ sgct::SGCTTracker * trackerPtr = NULL;
 sgct::SharedDouble curr_time(0.0);
 
 size_t myTextureHandle;
-<<<<<<< HEAD
-=======
-size_t myTextureHandle2;
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
 
 int main( int argc, char* argv[] )
 {
@@ -79,28 +75,20 @@ int main( int argc, char* argv[] )
 void myInitOGLFun()
 {
 
-  
-  
-  	  
+
+
+
 	sgct::TextureManager::instance()->setAnisotropicFilterSize(8.0f);
 	sgct::TextureManager::instance()->setCompression(sgct::TextureManager::S3TC_DXT);
 	sgct::TextureManager::instance()->loadTexure(myTextureHandle, "sphere", "sun.jpeg", true);
-<<<<<<< HEAD
 	sgct::TextureManager::instance()->loadTexure(myTextureHandle, "sphere2", "earth.jpeg", true);
 	sgct::TextureManager::instance()->loadTexure(myTextureHandle, "sphere3", "moon.jpeg", true);
-  
+
 	mySphere = new sgct_utils::SGCTSphere(0.2f, 30); //Jorden
 	mySphere1 = new sgct_utils::SGCTSphere(0.05f, 30); //Månen
 	mySphere2 = new sgct_utils::SGCTSphere(0.5f, 30); //Solen
-=======
-	sgct::TextureManager::instance()->loadTexure(myTextureHandle2, "sphere", "earth.jpeg", true);
-  
-	mySphere = new sgct_utils::SGCTSphere(0.2f, 20); //Jorden
-	mySphere1 = new sgct_utils::SGCTSphere(0.05f, 20); //Månen
-	mySphere2 = new sgct_utils::SGCTSphere(0.5f, 20); //Solen
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
-		
-	
+
+
 	glEnable( GL_DEPTH_TEST );
 	glEnable( GL_COLOR_MATERIAL );
 	glDisable( GL_LIGHTING );
@@ -108,31 +96,27 @@ void myInitOGLFun()
 	glEnable( GL_TEXTURE_2D );
 
 	glCullFace(GL_BACK);
-<<<<<<< HEAD
 	glFrontFace(GL_CCW);
-=======
-	glFrontFace(GL_CCW); //our p
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
-	
+
 	//only store the tracking data on the master node
 	if( gEngine->isMaster() )
 	{
 		size_t index = 0;
-		
+
 		//allocate shared data
 		for(size_t i = 0; i < sgct::Engine::getTrackingManager()->getNumberOfTrackers(); i++)
 		{
 			trackerPtr = sgct::Engine::getTrackingManager()->getTrackerPtr(i);
-			
+
 			//init the shared vector with identity matrixes
 			for(size_t j=0; j<trackerPtr->getNumberOfDevices(); j++)
 			{
 				devicePtr = trackerPtr->getDevicePtr(j);
-			
+
 				if( devicePtr->hasSensor() )
 				{
 					sharedTransforms.addVal( glm::mat4(1.0f) );
-					
+
 					//find the head sensor
 					if( sgct::Engine::getTrackingManager()->getHeadDevicePtr() == devicePtr )
 						sharedHeadSensorIndex.setVal(index);
@@ -171,17 +155,17 @@ void myPreSyncFun()
 		for(size_t i = 0; i < sgct::Engine::getTrackingManager()->getNumberOfTrackers(); i++)
 		{
 			trackerPtr = sgct::Engine::getTrackingManager()->getTrackerPtr(i);
-		
-			
+
+
 			/*
 				Loop trough all tracking devices (like headtracker, wand, stylus etc.)
 			*/
 			for(size_t j = 0; j < trackerPtr->getNumberOfDevices(); j++)
 			{
 				devicePtr = trackerPtr->getDevicePtr(j);
-				
+
 				ss << "Device " << i <<  "-" << j << ": " << devicePtr->getName() << "\n";
-				
+
 				if( devicePtr->hasSensor() )
 				{
 					sharedTransforms.setValAt( index, devicePtr->getWorldTransform() );
@@ -200,12 +184,13 @@ void myPreSyncFun()
 						<< "          rx=" << devicePtr->getEulerAngles().x << "\n"
 						<< "          ry=" << devicePtr->getEulerAngles().y << "\n"
 						<< "          rz=" << devicePtr->getEulerAngles().z << "\n";
+
 				}
 
 				if( devicePtr->hasButtons() )
 				{
 					ss << "\n     Buttons\n";
-					
+
 					for(size_t k=0; k < devicePtr->getNumberOfButtons(); k++)
 					{
 						ss << "          Button " << k << ": " << (devicePtr->getButton(k) ? "pressed" : "released") << "\n";
@@ -215,7 +200,7 @@ void myPreSyncFun()
 				if( devicePtr->hasAnalogs() )
 				{
 					ss << "\n     Analog axes\n";
-					
+
 					for(size_t k=0; k < devicePtr->getNumberOfAxes(); k++)
 					{
 						ss << "          Axis " << k << ": " << devicePtr->getAnalog(k) << "\n";
@@ -236,75 +221,45 @@ void myPreSyncFun()
 	Using a single viewport in stereo (3D) usually results in refresh rate of 120 Hz.
 */
 void myDrawFun()
-{	
+{
 	double speed = 25.0;
-	
-	
 
-<<<<<<< HEAD
+		//devicePtr = trackerPtr->getDevicePtr(size_t(0));
+
+
+//getNumberOfDevices();
+
 	glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByName("sphere") );
-	
+
 	glPushMatrix();
 	glRotated(curr_time.getVal() * speed, 0.0, -1.0, 0.0);
+/*
+	if (devicePtr->getPosition().x <= 0.5 && devicePtr->getPosition().y <= 0.5 && devicePtr->getPosition().z <= 0.5 )
+    {
+        glColor3f(1.0f,0.0f,0.0f);
+    }
+*/
 	mySphere2->draw();
+
 	glPopMatrix();
-	
-=======
-	//glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByName("box") );
-	
-	
-	
-	
+
 	glPushMatrix();
-		glRotated(curr_time.getVal() * speed, 0.0, -1.0, 0.0);	
-			//glColor3f(1.0f,1.0f,0.0f);
-			
-			glActiveTexture(GL_TEXTURE0);
-		
-glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByHandle(myTextureHandle) );
-		mySphere2->draw();
-		glPopMatrix();
-	
-	
-	glPushMatrix();
-	//glTranslatef(1.0, 0.0f, 0.0f);
-	glRotated(curr_time.getVal() * speed, 0.0, -1.0, 0.0);
-	glTranslatef(1.0, 0.0f,0.0f);
-	//glRotated(-curr_time.getVal() * speed, 0.0, -1.0, 0.0);
-	//glTranslatef(-1.0, 0.0f,0.0f);
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
-	
-	
-	glPushMatrix();
-<<<<<<< HEAD
 	glRotated(curr_time.getVal() * speed, 0.0, -1.0, 0.0);
 	glTranslatef(1.0, 0.0f,0.0f);
 	glRotated(curr_time.getVal() * speed*2, 0.0, -1.0, 0.0);
-	glPushMatrix();	
+	glPushMatrix();
 	glRotated(curr_time.getVal() * speed*4, 0.0, -1.0, 0.0);
 	glTranslatef(0.3, 0.1f, 0.0f);
 	glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByName("sphere3") );
-=======
-	//glTranslatef(0.3, 0.1f, 0.0f);
-		
-	glRotated(curr_time.getVal() * speed*2, 0.0, -1.0, 0.0);
-	glTranslatef(0.3, 0.1f, 0.0f);
-	glRotated(curr_time.getVal() * speed*2, 0.0, -1.0, 0.0);
-	//glColor3f(0.0f,1.0f,0.0f);
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
 	mySphere1->draw();
 	glPopMatrix();
-	
-	//glColor3f(1.0f,0.0f,0.0f);
-<<<<<<< HEAD
-	glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByName("sphere2") );
-=======
 
->>>>>>> c849537344bccbba56cb722b65e722e2c3fadbda
+	//glColor3f(1.0f,0.0f,0.0f);
+	glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByName("sphere2") );
 	mySphere->draw();
 	glPopMatrix();
-	
-	
+
+
 // 	//draw some yellow cubes in space
 // 	for( float i=-0.5f; i<=0.5f; i+=0.2f)
 // 		for(float j=-0.5f; j<=0.5f; j+=0.2f)
@@ -316,10 +271,14 @@ glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByHand
 // 			glPopMatrix();
 // 		}
 
+
+
+
+
 	//draw a cube and axes around each wand
 	for(size_t i = 0; i < sharedTransforms.getSize(); i++)
 	{
-		if(i != sharedHeadSensorIndex.getVal()) 
+		if(i != sharedHeadSensorIndex.getVal())
 		{
 			glLineWidth(2.0);
 
@@ -346,7 +305,7 @@ glBindTexture( GL_TEXTURE_2D, sgct::TextureManager::instance()->getTextureByHand
 	//draw text
 	float textVerticalPos = static_cast<float>(gEngine->getActiveWindowPtr()->getYResolution()) - 100.0f;
 	int fontSize = 12;
-	
+
 	glColor3f(1.0f, 1.0f, 1.0f);
 	sgct_text::print(sgct_text::FontManager::instance()->getFont( "SGCTFont", fontSize ),
 		120.0f, textVerticalPos,
